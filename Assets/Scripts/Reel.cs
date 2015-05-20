3005025c3e2bd3a314fbacfcf319a6c85016dc70
paulburtonplaor <paul.burton@plaor.com>
2015-05-20 11:15:38 -0400

    Some quick proof of concepts.

---
+++ Assets/Scripts/Reel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Reel : MonoBehaviour {
+  private List<GameObject> symbols = new List<GameObject>();
+
+	void Start () {
+    for (int i = 0; i < 20; ++i) {
+      GameObject symbol1r = Resources.Load("symbol1") as GameObject;
+      GameObject symbol1 = Instantiate(symbol1r, new Vector3(0, i * 1, 0), Quaternion.identity) as GameObject;
+      symbols.Add(symbol1);
+
+      symbol1.transform.parent = transform;
+    }
+	}
+	
+	void Update () {
+	}
+}
---
+++ Assets/Src/Slots/Core/Machine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using HTTP;
+
+namespace Slots {
+  namespace Core {
+    public class Machine : MonoBehaviour {
+      void Start() {
+        StartCoroutine(Get());
+      }
+
+      void Update() {
+      }
+
+      IEnumerator Get() {
+        HTTP.Request someRequest = new HTTP.Request("get", "http://www.doubletapgames.biz/hwp/dev/poker/bridge/globalregistry");
+        someRequest.Send();
+
+        while (!someRequest.isDone) {
+          yield return null;
+        }
+
+        Debug.Log(someRequest.response.Text);
+      }
+    }
+  }
+}
